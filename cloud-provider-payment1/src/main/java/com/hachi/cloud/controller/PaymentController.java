46d614e58c4eda25baf76841883a43c330eb13a4
Joker764 <764203174@qq.com>
2020-10-05 14:05:56 +0800

    服务发现

--- cloud-provider-payment1/src/main/java/com/hachi/cloud/controller/PaymentController.java
+++ cloud-provider-payment1/src/main/java/com/hachi/cloud/controller/PaymentController.java
@@ -6,6 +6,8 @@ import com.hachi.cloud.server.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -22,6 +24,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/create")
     public CommonResult<Payment> create(@RequestBody Payment payment) {
@@ -43,5 +48,18 @@ public class PaymentController {
         } else {
             return new CommonResult<>(400, "查询失败");
         }
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        for (String service : discoveryClient.getServices()) {
+            log.info("Service: " + service);
+        }
+
+        for (ServiceInstance instance : discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE")) {
+            log.info("Instance Service Id:" + instance.getServiceId() + " , Host: " + instance.getHost() + ", Port: " + instance.getPort());
+        }
+
+        return discoveryClient;
     }
 }
