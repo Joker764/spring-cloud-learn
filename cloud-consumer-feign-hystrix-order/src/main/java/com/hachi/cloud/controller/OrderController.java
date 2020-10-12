c2252d3fecff19f81175a0c7ed9ae27726ded33b
Joker764 <764203174@qq.com>
2020-10-12 19:17:33 +0800

    Hystrix 客户端降级

--- cloud-consumer-feign-hystrix-order/src/main/java/com/hachi/cloud/controller/OrderController.java
+++ cloud-consumer-feign-hystrix-order/src/main/java/com/hachi/cloud/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.hachi.cloud.controller;
 
 import com.hachi.cloud.service.OrderService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +26,14 @@ public class OrderController {
     }
 
     @GetMapping("/payment/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_fallback", commandProperties = {
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     public String paymentInfo_Timeout(@PathVariable("id") Integer id) {
         return orderService.paymentInfo_Timeout(id);
+    }
+
+    public String paymentInfo_fallback(@PathVariable("id") Integer id) {
+        return "Consumer Timeout, Please try it later... -_-";
     }
 }
